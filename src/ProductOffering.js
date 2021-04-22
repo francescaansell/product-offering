11d5b0fb02843d7663a52d33c945e546f41b5b3e
Francesca Ansell <fla5029@psu.edu>
2021-04-21 23:43:23 -0400

    move words

--- src/ProductOffering.js
+++ src/ProductOffering.js
@@ -1,9 +1,11 @@
 import { html, css, LitElement } from 'lit-element';
-/** import { images } from '../assets/Themes/Images'; */
+/** import { images } from '../assets/Themes/Images'; 
 
 /**
  * Figure out how to import images/icon
  * Put images/icons in the tag 
+ * Figure out how to underline just the first word
+ * Fix word overlapping
  */
 
 export class ProductOffering extends LitElement {
@@ -16,22 +18,24 @@ export class ProductOffering extends LitElement {
       }
       #grid-container {
         display: inline-grid;
-        grid-column-gap: 25px;
-        grid-row-gap: 25px; 
+        grid-column-gap: 10px;
+        grid-row-gap: 10px; 
         grid-template-columns: 50% 50%;
         border-style: solid;
         border-color: #e8f4f8;
         background-color: #e8f4f8;
-        padding-left: 5%;
-        padding-right: 5%;
-        margin-left: 3%;
-        margin-right: 3%;
+        padding: 2%;
       }
-      #grid-item {
-        
-        padding-left: 10%;
-        padding-right: 10%;
-        border-style: solid;
+      #grid-outer-item {
+        padding: 5%;
+        display: grid;
+        grid-template-columns: 30% 70%;
+      }
+      .underline {
+        text-decoration: underline;
+      }
+      .wordwrap {
+        word-wrap: normal;
       }
     `;
   }
@@ -48,7 +52,7 @@ export class ProductOffering extends LitElement {
   constructor() {
     super();
     /** this.image = images.placeholder; */
-    this.title = ["Real-world Projects from industry experts", "Techincal mentor support", "Career services" , "Flexible learning program"];
+    this.title = ["Title", "Title", "Title" , "Title"];
     /** this.icon = productManagement;  * */
     this.description = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida sit amet sem id hendrerit. Ut varius maximus mi, pretium cursus libero. Maecenas hendrerit lacinia finibus.", 
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris gravida sit amet sem id hendrerit. Ut varius maximus mi, pretium cursus libero. Maecenas hendrerit lacinia finibus.",
@@ -60,21 +64,33 @@ export class ProductOffering extends LitElement {
   render() {
     return html`
       <div id="grid-container">
-        <div id="grid-item">
-          <h4> ${this.title[0] } </h4>
-          <p> ${this.description[0] } </p>
+        <div id="grid-outer-item">
+          <img src="../assets/images/placeholder1.jpeg" alt="Need to put somthing here" height=150px width=200px/>
+          <div>
+            <h4> <span class="underline"> ${this.title[0][0]} </span> ${this.title[0] } </h4>
+            <p> ${this.description[0] } </p>
+          </div>
         </div>
-        <div id="grid-item">
-          <h4> ${this.title[1] } </h4>
-          <p> ${this.description[1] } </p>
+        <div id="grid-outer-item">
+          <img src="../assets/images/placeholder3.jpeg" alt="Need to put somthing here" height=150px width=200px/>
+          <div>
+            <h4> ${this.title[1] } </h4>
+            <p> ${this.description[1] } </p>
+          </div>
         </div>
-        <div id="grid-item">
-          <h4>${this.title[2] } </h4>
-          <p> ${this.description[2] } </p>
+        <div id="grid-outer-item">
+          <img src="../assets/images/placeholder2.jpeg" alt="Need to put somthing here" height=150px width=200px/>
+          <div>
+            <h4>${this.title[2] } </h4>
+            <p> ${this.description[2] } </p>
+          </div>
         </div>
-        <div id="grid-item">
-          <h4>${this.title[3] } </h4>
-          <p> ${this.description[3] } </p>
+        <div id="grid-outer-item">
+          <img src="../assets/images/placeholder4.jpeg" alt="Need to put somthing here" height=150px width=200px/>
+          <div class="wordwrap">
+            <h4>${this.title[3] } </h4>
+            <p> ${this.description[3] } </p>
+          </div>
         </div>
       </div>
 
