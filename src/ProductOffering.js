8cf46d47eaf7c2292d00a82211821e17cb22b6f2
Francesca <fla5029@psu.edu>
2021-04-27 12:59:37 -0400

    working slot and simple icons

--- src/ProductOffering.js
+++ src/ProductOffering.js
@@ -1,64 +1,66 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable lit/binding-positions */
 import { html, css, LitElement } from 'lit-element';
 import { IntersectionObserverMixin } from '@lrnwebcomponents/intersection-element/lib/IntersectionObserverMixin.js';
-// import { SimpleColors } from "@lrnwebcomponents/simple-colors/simple-colors.js";
+import { SimpleColors } from '@lrnwebcomponents/simple-colors/simple-colors.js';
 import '@lrnwebcomponents/simple-icon/simple-icon.js';
 import '@lrnwebcomponents/simple-icon/lib/simple-icons.js';
 
-export class ProductOffering extends IntersectionObserverMixin(LitElement) {
+export class ProductOffering extends IntersectionObserverMixin(SimpleColors) {
   // @todo make sure we ship this as an array of styles
   static get styles() {
-    return css`
-      :host {
-        display: block;
-        padding: var(--product-offering-padding, 25px);
-        color: var(--product-offering-text-color, #000);
-        font-family: Verdana, sans-serif;
-      }
-      .grid-outer-item {
-        padding: 5%;
-        display: grid;
-        grid-template-columns: 30% 70%;
-      }
-      .underline {
-        text-decoration: underline;
-      }
-      .wordwrap {
-        word-wrap: normal;
-      }
-      p {
-        color: #808080;
-        font-size: 9pt;
-        padding: 20px;
-        padding-left: 60px;
-        margin: 0;
-      }
-      div {
-        padding: 5px;
-      }
-      .banner {
-        display: flex;
-      }
-      #simple-icon {
-        padding: 10px;
-        height: 25px;
-        width: 25px;
-      }
-      .icon-background {
-        background-color: white;
-        border-radius: 50%;
-        padding: 2px;
-        margin: 5px;
-        margin-right: 10px;
-        box-shadow: 10px 10px 25px 0 rgb(0 0 0 / 10%);
-      }
-      .square {
-        height: 150px;
-        width: 200px;
-        border-radius: 2%;
-      }
-    `;
+    return [
+      ...super.styles,
+
+      css`
+        :host {
+          display: block;
+          padding: var(--product-offering-padding, 25px);
+          color: var(--product-offering-text-color, #000);
+          font-family: Verdana, sans-serif;
+        }
+        .container {
+          padding: 5%;
+          display: grid;
+          grid-template-columns: 30% 70%;
+        }
+        .image {
+          height: 150px;
+          width: 200px;
+          border-radius: 2%;
+        }
+        .square {
+          margin-left: 10%;
+        }
+        #simple-icon {
+          padding: 10px;
+          height: 25px;
+          width: 25px;
+        }
+        .icon-background {
+          background-color: white;
+          border-radius: 50%;
+          padding: 2px;
+          margin: 5px;
+          margin-right: 10px;
+          box-shadow: 10px 10px 25px 0 rgb(0 0 0 / 10%);
+        }
+        .squareTitle {
+          display: flex;
+        }
+        .underline {
+          text-decoration: underline;
+        }
+        .sqaureDescription {
+          color: var(--simple-colors-default-theme-black-12);
+          font-size: 12pt;
+          padding: 20px;
+          padding-left: 60px;
+          margin: 0;
+        }
+      `,
+    ];
   }
 
   static get properties() {
@@ -70,9 +72,8 @@ export class ProductOffering extends IntersectionObserverMixin(LitElement) {
       ...props,
       title: { type: String },
       description: { type: String },
-      // description: { type: Slot },
       source: { type: String },
-      icon: { type: String }, // @todo @lrnwebcomponents/simple-icon would be the way to go here
+      icon: { type: String },
       _titleOne: { type: String },
       _titleTwo: { type: String },
       accentColor: { type: String },
@@ -82,6 +83,8 @@ export class ProductOffering extends IntersectionObserverMixin(LitElement) {
   constructor() {
     super();
     this.alt = '';
+    this.accentColor = 'pink';
+    this.dark = false;
   }
 
   updated(changedProperties) {
@@ -100,27 +103,35 @@ export class ProductOffering extends IntersectionObserverMixin(LitElement) {
 
   render() {
     return html`
-      <div class="grid-outer-item">
+      <!-- Biggest container -->
+      <div class="container">
         <img
-          class="square"
+          class="image"
           src="${this.source}"
           alt="${this.alt}"
           height="150px"
           width="200px"
         />
-        <div>
-          <div class="banner">
+
+        <div class="square">
+          <!-- Icon, header -->
+          <div class="squareTitle">
+            <!-- icon -->
             <div class="icon-background">
               <simple-icon id="simple-icon" icon="${this.icon}"></simple-icon>
             </div>
+            <!-- header -->
             <h4>
               <span class="underline">${this._titleOne}</span>&nbsp;<span
                 >${this._titleTwo}</span
               >
             </h4>
           </div>
-          <p>${this.description}</p>
-          <!--  <slot name = description > Example Slot </slot> -->
+
+          <!-- descripton -->
+          <div class="sqaureDescription">
+            <slot name="description"> Example Slot </slot>
+          </div>
         </div>
       </div>
     `;
